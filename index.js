27b82c2ea93ce9d2c6cd255ad5689a958df3448f
farhan hossain <farhanrufon99@gmail.com>
2023-07-03 19:27:23 -0400

    modifies index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
+
+const input = document.getElementById('button');
+  console.log("input",input);
 function addingEventListener() {
+  
+  input.addEventListener("click",function(event){
+    alert('I was clicked!')
+  });
 }
+
+//input.addEventListener("click",addingEventListener);
